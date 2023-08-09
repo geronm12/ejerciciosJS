70a81e180de6b5af35c7fd9b06c70f1ec5dae045
geronm12 <gerolpz01@gmail.com>
2023-08-09 18:09:21 -0300

    agregado ordenar de mayor a menor y de menor a mayor

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
 //    0        n-1  (length)
 let numeros = [];
+const acciones = [
+  "A - Agregar",
+  "E - Eliminar",
+  "L - Leer",
+  "T - Sumatoria Total",
+  "O - Ordenar",
+  "S - Salir",
+];
 let continuar = true;
+
+let message = "Seleccione la operación que desea realizar.";
+
+acciones.forEach((accion) => {
+  message += "\n" + accion;
+});
+
 while (continuar) {
-  const eleccion = prompt(
-    "Seleccione la operación que desea realizar. \n A - Agregar  \n E - Eliminar \n L - Leer \n S - Salir"
-  );
+  const eleccion = prompt(message);
   switch (eleccion.toUpperCase()) {
     case "A":
       let numero = prompt("Ingrese un número por favor");
       numero = +numero;
-      numeros.push(numero);
+      if (!Number.isNaN(numero)) {
+        numeros.push(numero);
+      }
       break;
     case "E":
       numeros.pop();
@@ -18,7 +33,29 @@ while (continuar) {
       alert(numeros);
       break;
     case "T":
-      alert("Total es:");
+      let acumulador = 0;
+      numeros.forEach((elemento) => {
+        acumulador += elemento;
+      });
+      alert("La sumatoria total es: " + acumulador);
+      break;
+    case "O":
+      let resp = prompt(
+        "Desea ordenar: \n 1- Mayor a menor \n 2 - Menor a Mayor"
+      );
+      if (resp === "1") {
+        alert(
+          numeros.sort(function (a, b) {
+            return a - b;
+          })
+        );
+      } else if (resp === "2") {
+        alert(
+          numeros.sort(function (a, b) {
+            return b - a;
+          })
+        );
+      }
       break;
     case "S":
       let respuesta = prompt("Está seguro que desea finalizar? y/n");
@@ -29,5 +66,7 @@ while (continuar) {
       break;
   }
 }
+
+console.log(["Manzana", "Banana", "Pera"].sort());
 
 console.log(numeros);
